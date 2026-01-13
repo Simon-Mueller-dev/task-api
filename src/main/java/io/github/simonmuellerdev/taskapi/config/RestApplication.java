7f79010b4247e6508b9afff1bff6c6dcfce7bbc7
Simon-Mueller-dev <simonm21303@gmail.com>
2026-01-13 20:26:33 +0100

    Initialize Maven project with Jakarta EE REST setup

---
+++ src/main/java/io/github/simonmuellerdev/taskapi/config/RestApplication.java
@@ -0,0 +1,9 @@
+package io.github.simonmuellerdev.taskapi.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
+
---
+++ src/main/java/io/github/simonmuellerdev/taskapi/model/task.java
@@ -0,0 +1,4 @@
+package io.github.simonmuellerdev.taskapi.model;
+
+public class task {
+}
