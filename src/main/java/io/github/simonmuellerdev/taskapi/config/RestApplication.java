9bca229dfe9802b9d10e2c816f1ff311fbf2845d
Simon-Mueller-dev <simonm21303@gmail.com>
2026-03-13 21:01:23 +0100

    feat: integrate OpenAPI specification with Swagger for Jakarta REST API

--- src/main/java/io/github/simonmuellerdev/taskapi/config/RestApplication.java
+++ src/main/java/io/github/simonmuellerdev/taskapi/config/RestApplication.java
@@ -2,12 +2,29 @@ package io.github.simonmuellerdev.taskapi.config;
 
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
-@ApplicationPath("/api")
+@OpenAPIDefinition(
+        info = @Info(
+        title = "Task API",
+        version = "1.1.0",
+        description = "A simple RESTful Task management API built with Jakarta EE 10"
+         )
+        )
+
+@ApplicationPath("/api/v1")
 public class RestApplication extends ResourceConfig {
 
     public RestApplication() {
         packages("io.github.simonmuellerdev.taskapi.resource");
+        // Register the Swagger/OpenAPI resources
+        register(OpenApiResource.class);
+        register(AcceptHeaderOpenApiResource.class);
+
+        property("jersey.config.server.wadl.disableWadl", true);
     }
 }
 
